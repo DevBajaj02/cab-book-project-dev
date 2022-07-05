5cb5b425550cd35daa7cbd5aa7d59d19e9eea4ac
Dev <devbajaj.bits@gmail.com>
2022-07-05 20:38:01 +0530

    First Version

---
+++ app/src/main/java/com/devprojectudemy/cabbookingdev/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devprojectudemy.cabbookingdev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
